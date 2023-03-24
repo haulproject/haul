e35f8f1735418eb500ff6aba02c2af63b33c68ac
Victor Lacasse-Beaudoin <victor.lacassebeaudoin@gmail.com>
2023-03-23 22:31:06 -0400

    Associate componentCreate with /v1/component POST
    Currently only allows adding a single component from args, will error
    out if used with anything other than 1 arg.

--- cmd/componentCreate.go
+++ cmd/componentCreate.go
@@ -4,13 +4,16 @@ Copyright © 2023 The Haul Authors
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"codeberg.org/haulproject/haul/types"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // componentCreateCmd represents the componentCreate command
@@ -20,6 +23,9 @@ var componentCreateCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
 			log.Fatal("Must specify at least 1 component to create")
+		}
+		if len(args) > 1 {
+			log.Fatal("Can currently only insert 1 component at a time")
 		}
 
 		var components []types.Component
@@ -36,7 +42,8 @@ var componentCreateCmd = &cobra.Command{
 			components = append(components, component)
 
 		}
-		result, err := json.Marshal(components)
+
+		_, err := json.Marshal(components)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -46,7 +53,32 @@ var componentCreateCmd = &cobra.Command{
 				os.Stderr.WriteString("component.Name cannot be empty")
 			}
 		}
-		fmt.Printf("%s\n", string(result))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		currentComponent, err := json.Marshal(components[0])
+
+		endpoint := fmt.Sprintf("%s://%s:%d",
+			viper.GetString("api.protocol"),
+			viper.GetString("api.host"),
+			viper.GetInt("api.port"),
+		)
+		request := fmt.Sprintf("%s%s", endpoint, "/v1/component")
+
+		resp, err := http.Post(request, "application/json",
+			bytes.NewBuffer(currentComponent))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var res map[string]interface{}
+
+		json.NewDecoder(resp.Body).Decode(&res)
+
+		fmt.Println(res["message"])
 	},
 }
 
