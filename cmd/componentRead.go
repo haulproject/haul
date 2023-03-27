d82a5a88f4db37b969487c2cdff8cfbdad88c283
Victor Lacasse-Beaudoin <victor.lacassebeaudoin@gmail.com>
2023-03-27 16:40:21 -0400

    Add `get` alias to componentReadCmd

--- cmd/componentRead.go
+++ cmd/componentRead.go
@@ -13,9 +13,10 @@ import (
 
 // componentReadCmd represents the componentRead command
 var componentReadCmd = &cobra.Command{
-	Use:   "read OBJECT_ID",
-	Short: "Prints values of component identified by OBJECT_ID",
-	Args:  cobra.ExactArgs(1),
+	Use:     "read OBJECT_ID",
+	Aliases: []string{"get"},
+	Short:   "Prints values of component identified by OBJECT_ID",
+	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		result, err := api.Call(api.GET, fmt.Sprintf("/v1/component/%s", args[0]))
 		if err != nil {
