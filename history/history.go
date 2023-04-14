3520829a7c3c488e27815708d19e671759e8f6c0
Victor <victor.lacassebeaudoin@proton.me>
2023-04-13 23:10:33 -0400

    Add LogCreate and LogDelete

--- history/history.go
+++ history/history.go
@@ -1,8 +1,30 @@
 package history
 
-import "log"
+import (
+	"fmt"
+	"time"
+)
 
-func Log(message string) error {
-	log.Printf("history: %s\n", message)
+func Log(id, message string) error {
+	fmt.Printf("[%s] %s: %s\n", time.Now(), id, message)
+	//TODO insert message into database as a document in the 'histories' collection
+	return nil
+}
+
+func LogCreate(id, name string) error {
+	err := Log(id, fmt.Sprintf("'%s' created", name))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func LogDelete(id string) error {
+	err := Log(id, "deleted")
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
