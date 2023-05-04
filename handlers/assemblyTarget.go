a0097f7e9bc4abfd41db55392045f689cd88b05d
Victor <victor.lacassebeaudoin@proton.me>
2023-05-04 02:57:25 -0400

    Implement target unset routes

--- handlers/assemblyTarget.go
+++ handlers/assemblyTarget.go
@@ -1,12 +1,14 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"codeberg.org/haulproject/haul/db"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -55,7 +57,59 @@ func HandleV1AssemblyTarget(c echo.Context) error {
 }
 
 func HandleV1AssemblyTargetUnset(c echo.Context) error {
-	return c.JSON(http.StatusNotImplemented, map[string]string{"message": "Not Implemented"})
+	assemblyID, err := primitive.ObjectIDFromHex(c.Param("assembly"))
+	if err != nil {
+		if err == primitive.ErrInvalidHex {
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"message": fmt.Sprintf("%s", err),
+			})
+		}
+
+		log.Println(err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"message": fmt.Sprintf("Internal server error"),
+		})
+	}
+
+	empty, err := primitive.ObjectIDFromHex("000000000000000000000000")
+	if err != nil {
+		log.Println(err)
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"message": fmt.Sprintf("Internal server error"),
+		})
+	}
+
+	update := bson.D{
+		primitive.E{
+			Key: "$set", Value: bson.D{
+				bson.E{Key: "target", Value: empty}},
+		},
+	}
+
+	result, err := db.UpdateFromID("assemblies", assemblyID, update)
+	if err != nil || result == nil {
+		// ErrNoDocuments means that the filter did not match any documents in
+		// the collection.
+		if err == mongo.ErrNoDocuments {
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"message": "No document with specified ObjectID",
+			})
+		}
+
+		// other
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"message": err.Error(),
+		})
+	}
+
+	message, err := json.Marshal(result)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"message": "Error marshalling result",
+		})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"message": string(message)})
 }
 
 func HandleV1AssemblyTargetSet(c echo.Context) error {
